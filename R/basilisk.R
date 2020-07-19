30a4bae7b96aabefe5b9b4049e955dba2248b9de
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-07-19 20:33:51 +0100

    1.5.0 for windows

--- R/basilisk.R
+++ R/basilisk.R
@@ -4,7 +4,7 @@ python_env <- BasiliskEnvironment(
     packages = c(
       "opentsne==0.4.3",
       "scikit-learn=0.23.1",
-      "scipy=1.5.1",
+      if (basilisk.utils::isWindows()) "scipy=1.5.0" else "scipy=1.5.1",
       "numpy=1.19.0"
     )
 )
