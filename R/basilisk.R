a1b91d4998ddf5aa7ad051f6ec7339a3c41901ae
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-02-09 20:40:31 +0000

    Add extra envs to GHA, update conda pkgs

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,9 +2,9 @@ python_env <- BasiliskEnvironment(
     "fitsne",
     pkgname = "snifter",
     packages = c(
-      "opentsne==0.4.3",
-      "scikit-learn=0.23.1",
-      if (basilisk.utils::isWindows()) "scipy=1.5.0" else "scipy=1.5.1",
-      "numpy=1.19.0"
+      "opentsne=0.5.1",
+      "scikit-learn=0.24.1",
+      if (basilisk.utils::isWindows()) "scipy=1.6.0" else "scipy=1.6.0",
+      "numpy=1.20.0"
     )
 )
