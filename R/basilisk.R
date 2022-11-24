edfef2953deb914405b67cf1faa2187f21a2285d
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2022-11-24 12:46:58 +0000

    Roll back python v for #10

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,10 +2,10 @@ python_env <- BasiliskEnvironment(
     "fitsne",
     pkgname = "snifter",
     packages = c(
-      "python=3.10.6",
+      "python=3.9.13",
       "opentsne=0.6.2",
       "scikit-learn=1.1.2",
-      if (basilisk.utils::isWindows()) "scipy=1.7.3" else "scipy=1.7.3",
+      "scipy=1.7.3",
       "numpy=1.22.0"
     )
 )
