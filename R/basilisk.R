99e9f4d6e0d68343c7d62b1eb46ca9cf8575b6d4
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-07-17 10:52:48 +0100

    Bump version and update spec

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,10 +2,10 @@ python_env <- BasiliskEnvironment(
     "fitsne",
     pkgname = "snifter",
     packages = c(
-      "python=3.10.6",
+      "python=3.12.10",
       "opentsne=1.0.2",
-      "scikit-learn=1.6.1",
-      "scipy=1.15.2",
-      "numpy=2.2.6"
+      "scikit-learn=1.7.0",
+      "scipy=1.16.0",
+      "numpy=2.3.1"
     )
 )
